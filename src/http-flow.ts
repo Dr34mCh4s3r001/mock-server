c0c8ececa08581676b688894210520eb8cb86529
GhostE <eddy.pathit@gmail.com>
2025-05-19 20:53:28 +0700

    feat: support path param

--- src/http-flow.ts
+++ src/http-flow.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { containNodes, generateNodes, JsonFlowSpec, readFlowSpec } from "./node/json-flow-spec";
 import { FlowExecutor } from "./node/flow-executor";
 import { HTTPMethod } from "./node/http-request-node";
+import { match } from "path-to-regexp";
 
 const cwd = process.cwd();
 
@@ -33,17 +34,12 @@ export async function httpFlowResolver(req: Request, res: Response) {
     console.log('search flow result: ', flow)
     // special for http need to supply req
     const nodes = generateNodes(flow)(req)
+    const startNode = flow.nodes.find(n => n.type === 'start')!
     // console.log('generated nodes:', nodes)
-    const flowEx = new FlowExecutor(nodes, flow.nodes.find(n => n.type === 'start')!.id);
-    const context = { req, res }
+    const flowEx = new FlowExecutor(nodes, startNode.id);
+    const pathParams = getPathParams(startNode.config.path, reqPath)
+    const context = { req, pathParams, res }
     const result = await flowEx.run(context);
-    // assume all req flow have sent
-    // if (!result) {
-    //     res.send('ok')
-    // } else {
-    //     res.json(result)
-    // }
-    // console.log('context', context)
 }
 
 export function reloadHttpReqFlowCache() {
@@ -72,10 +68,24 @@ function findMatchPath(method: HTTPMethod, path: string, spec: JsonFlowSpec): bo
     const result = spec.nodes
         .filter(n => n.node === "httpRequest")
         // .map(n => { console.log(n); return n })
-        .filter(n => n.config.path === path && n.config.method === method)
+        .filter(n => matchPath(n.config.path, path) && n.config.method === method)
     return result.length === 1 ? true : false
 }
 
 function last(arr: any[]) {
     return arr[arr.length - 1];
+}
+
+function matchPath(template: string, path: string): boolean {
+  const matcher = match(template, { decode: decodeURIComponent });
+  const result = matcher(path);
+  if (!result) return false;
+  return true
+}
+
+function getPathParams(template: string, path: string): null | Record<string, string> {
+  const matcher = match(template, { decode: decodeURIComponent });
+  const result = matcher(path);
+  if (!result) return null;
+  return result.params as Record<string, string>;
 }
