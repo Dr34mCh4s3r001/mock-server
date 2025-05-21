5f0e08659f144bffd0f0c82dca44d6b985abdc82
Errortime <larb26656@gmail.com>
2025-05-21 09:02:38 +0700

    chore: remove global.ts

--- server/src/global.ts
+++
@@ -1,5 +0,0 @@
-import { AppConfig } from './app-config';
-import { FlowConfigService } from './flow';
-
-export const appConfig = new AppConfig();
-export const flowConfig = new FlowConfigService(appConfig.flowDir);
